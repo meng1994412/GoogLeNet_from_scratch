5299928bad3a7409f36e88aadb3ab085147bc892
Sunny Meng <sunnym412@gmail.com>
2019-02-06 15:24:49 -0600

    feat: modified the path to the datasets

--- config/tiny_imagenet_config.py
+++ config/tiny_imagenet_config.py
@@ -2,17 +2,17 @@
 from os import path
 
 # define the paths to the training and validation directories
-TRAIN_IMAGES = "datasets/tiny-imagenet-200/train"
-VAL_IMAGES = "datasets/tiny-imagenet-200/val/images"
+TRAIN_IMAGES = "../datasets/tiny-imagenet-200/train"
+VAL_IMAGES = "../datasets/tiny-imagenet-200/val/images"
 
 # define the path to the file that maps validation filenames to
 # their corresponding class labels
-VAL_MAPPINGS = "datasets/tiny-imagenet-200/val/val_annotations.txt"
+VAL_MAPPINGS = "../datasets/tiny-imagenet-200/val/val_annotations.txt"
 
 # define the paths to the WordNet hierarchy files
 # which are used to generate class labels
-WORDNET_IDS = "datasets/tiny-imagenet-200/wnids.txt"
-WORD_LABELS = "datasets/tiny-imagenet-200/words.txt"
+WORDNET_IDS = "../datasets/tiny-imagenet-200/wnids.txt"
+WORD_LABELS = "../datasets/tiny-imagenet-200/words.txt"
 
 # since we do not have access to testing data we need to
 # take a number of images from the training data and use it instead
@@ -20,9 +20,9 @@ NUM_CLASSES = 200
 NUM_TEST_IMAGES = 50 * NUM_CLASSES
 
 # define the path to the output training, validation, and testing HDF5 files
-TRAIN_HDF5 = "datasets/tiny-imagenet-200/hdf5/train.hdf5"
-VAL_HDF5 = "datasets/tiny-imagenet-200/hdf5/val.hdf5"
-TEST_HDF5 = "datasets/tiny-imagenet-200/hdf5/test.hdf5"
+TRAIN_HDF5 = "../datasets/tiny-imagenet-200/hdf5/train.hdf5"
+VAL_HDF5 = "../datasets/tiny-imagenet-200/hdf5/val.hdf5"
+TEST_HDF5 = "../datasets/tiny-imagenet-200/hdf5/test.hdf5"
 
 # define the path to the dataset mean
 DATASET_MEAN = "output/tiny-imagenet-200-mean.json"
